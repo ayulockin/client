6f2627440b2cfac36170a3b85fa8f97a17dd09cd
ayulockin <mein2work@gmail.com>
2022-07-26 09:21:06 +0000

    tablesbuilder init

--- wandb/integration/keras/__init__.py
+++ wandb/integration/keras/__init__.py
@@ -5,6 +5,6 @@ Use the `WandbCallback` to add `wandb` logging to any `Keras` model.
 """
 
 from .keras import WandbCallback
-from .kerasv2 import *
+from .table_builder import TablesBuilder
 
-__all__ = ["WandbCallback"]
+__all__ = ["WandbCallback", "TablesBuilder"]
