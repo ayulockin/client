dbd75327b52172229c3341978ce5366628537ebc
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2022-08-09 15:16:30 +0000

    added basic metrics logger callback

---
+++ wandb/integration/keras/callbacks/metrics_logger.py
@@ -0,0 +1,27 @@
+import wandb
+from typing import Optional
+
+import tensorflow as tf
+from tensorflow.keras import callbacks
+
+from ..keras import patch_tf_keras
+
+
+patch_tf_keras()
+
+
+class WandBMetricsLogger(callbacks.Callback):
+    def __init__(self, log_batch_frequency: Optional[int] = None):
+        self.log_batch_frequency = log_batch_frequency
+
+    def on_epoch_end(self, epoch, logs={}):
+        wandb.log({"epoch": epoch}, commit=False)
+        wandb.log(logs, commit=True)
+
+    def on_batch_end(self, batch, logs={}):
+        if self.log_batch_frequency and batch % self.log_batch_frequency == 0:
+            wandb.log(logs, commit=True)
+
+    def on_train_batch_end(self, batch, logs={}):
+        if self.log_batch_frequency and batch % self.log_batch_frequency == 0:
+            wandb.log(logs, commit=True)
